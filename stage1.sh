862f2cc368eab5bad50efc9232a33acdd433b232
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2025-10-07 12:57:21 -0500

    chore: Move away from PyPi `tfluna-driver` and to direct git -> asj fork

--- stage1.sh
+++ stage1.sh
@@ -104,7 +104,7 @@ apt-get install -y gh dunst attr git build-essential firefox \
     python3-dev mypy python3-mypy python3-flask python3-flask-socketio \
     python3-picamera2 python3-smbus python3-rpi.gpio
  
-pip3 install mpu6050-raspberrypi git+https://github.com/AerospaceJam/bmp180.git tfluna-driver --break-system-packages
+pip3 install mpu6050-raspberrypi git+https://github.com/AerospaceJam/bmp180.git git+https://github.com/AerospaceJam/tfluna.git --break-system-packages
 
 echo "Installing uv for user ${SDK_USERNAME}..."
 sudo -H -u "${SDK_USERNAME}" bash -c 'curl -LsSf https://astral.sh/uv/install.sh | sh'
